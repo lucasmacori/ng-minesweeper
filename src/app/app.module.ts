2a8f94ffd6249cac6847c4a47de47ae5ccd59766
LegitBaboulinet <pro@lucasmacori.fr>
2020-07-18 00:50:09 +0200

    (feature) Custom game selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MsGridComponent } from './../components/ms-grid/ms-grid.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MsHeaderComponent } from '../components/ms-header/ms-header.component';
 import { MenuComponent } from '../components/menu/menu.component';
+import { OnlyNumbersDirective } from '../directives/only-numbers.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MsGridComponent,
     MsHeaderComponent,
-    MenuComponent
+    MenuComponent,
+    OnlyNumbersDirective
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/menu/menu.component.ts
+++ src/components/menu/menu.component.ts
@@ -1,22 +1,41 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { IconDefinition, faWrench, faBomb } from '@fortawesome/free-solid-svg-icons';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { IconDefinition, faWrench, faBomb, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { MenuSelection } from './../../models/menuSelection.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   @Output() selection: EventEmitter<MenuSelection>;
 
   // Icons
   public faBomb: IconDefinition = faBomb;
   public faWrench: IconDefinition = faWrench;
+  public faArrowLeft: IconDefinition = faArrowLeft;
+
+  // Custom menu
+  public showCustomMenu: boolean;
+  public customFormGroup: FormGroup;
+  public customSubmitClasses: object;
 
   constructor() {
     this.selection = new EventEmitter<MenuSelection>();
+    this.showCustomMenu = false;
+  }
+
+  public ngOnInit(): void {
+    this.customFormGroup = new FormGroup({
+      cols: new FormControl('', [ Validators.min(4), Validators.max(99999), Validators.required ]),
+      rows: new FormControl('', [ Validators.min(4), Validators.max(99999), Validators.required ])
+    });
+    this.customSubmitClasses = { invalid: !this.customFormGroup.valid };
+    this.customFormGroup.valueChanges.subscribe(() => {
+      this.customSubmitClasses = { invalid: !this.customFormGroup.valid };
+    });
   }
 
   public selectMode(mode: string, width?: number, height?: number): void {
@@ -54,4 +73,15 @@ export class MenuComponent {
     this.selection.emit(menuSelection);
   }
 
+  public selectCustomMode(): void {
+    if (this.customFormGroup.valid) {
+      const width = this.customFormGroup.value.cols;
+      const height = this.customFormGroup.value.rows;
+      this.selectMode('CUSTOM', width, height);
+    }
+  }
+
+  public toggleCustomMenu(): void {
+    this.showCustomMenu = !this.showCustomMenu;
+  }
 }
